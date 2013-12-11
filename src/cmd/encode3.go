51ec26109aa2c9825e2cf50db872cc7567eec23c
Daigo Ikeda <ikeda@knightso.co.jp>
2013-12-11 14:55:18 +0900

    encode json sample3

---
+++ src/cmd/encode3.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Book struct {
+	Title   string   `json:"title"`
+	Price   int      `json:"price,omitempty"`
+	Authors []string `json:"authors"`
+	Secret  string   `json:"-"`
+}
+
+type Bookshelf struct {
+	Books []Book `json:"books"`
+}
+
+func main() {
+	bs := Bookshelf{
+		[]Book{
+			{"Gopher vs JSON!", 1000, []string{"山田　太郎", "鈴木　一郎"}, "KKK"},
+			{"Go phrasebook", 2000, []string{"ほげ　ぼすけて"}, "xxx"},
+			{"Go! Gopher", 3000, []string{"テスト"}, "zzz"},
+			{"", 0, []string{"hoge"}, "hoge"},
+		},
+	}
+	b, err := json.MarshalIndent(bs, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", b)
+}
