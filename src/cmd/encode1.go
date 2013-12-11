3bc9c1a63b0c0867b2901a4da5931c3448e9bf22
Daigo Ikeda <ikeda@knightso.co.jp>
2013-12-11 14:06:03 +0900

    encode json sample2

---
+++ src/cmd/encode1.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Book struct {
+	Title   string
+	Price   int
+	Authors []string
+}
+
+type Bookshelf struct {
+	Books   []Book
+}
+
+func main(){
+	bs := Bookshelf{
+		[]Book{
+			{"Gopher vs JSON!", 1000, []string{"山田　太郎", "鈴木　一郎"}},
+			{"Go phrasebook", 2000, []string{"ほげ　ぼすけて"}},
+			{"Go! Gopher", 3000, []string{"テスト"}},
+		},
+	}
+	b, err := json.MarshalIndent(bs, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", b)
+}
+
---
+++ src/cmd/encode2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main(){
+	bs := map[string]interface{}{
+		"Books": []map[string]interface{} {
+			map[string]interface{} {
+				"Title" : "Gopher vs JSON!",
+				"Price" : 1000,
+				"Authors" : []string{"山田　太郎", "鈴木　一郎"},
+			},
+			map[string]interface{} {
+				"Title" : "Go phrasebook",
+				"Price" : 2000,
+				"Authors" : []string{"山田　太郎", "鈴木　一郎"},
+			},
+			map[string]interface{} {
+				"Title" : "Go! Gopher",
+				"Price" : 3000,
+				"Authors" : []string{"テスト"},
+			},
+		},
+	}
+	b, err := json.MarshalIndent(bs, "", "\t")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", b)
+}
+
